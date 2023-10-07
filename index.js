f1e44a579a092ecfbdec0d009fe9f3c43aecdc30
Sourav123-sys <khansourav58@gmail.com>
2023-10-07 20:16:07 +0600

    dummy data create

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const express = require('express');
 
-
+const port = 5000
 const app = express()
+
+const fortunes = require ("./data/fortunes.json")
+
+
+app.get('/fortunes', (req, res) => {
+
+    res.json(fortunes);
+})
+ 
+app.listen(port, () => {
+    console.log(`server is running on port number ${port}`);
+})
